2ed67e2a722eb18fa33bd168c98aa1e4c87824a1
dead inside <82109250+fast-typing@users.noreply.github.com>
2022-09-28 13:34:07 +0400

    полу рабочий

--- type.js
+++ type.js
@@ -1,19 +1,35 @@
-var text = document.getElementById('text').innerText
-
-text = text.slice(0, text.length).split(' ')
+var text = document.getElementById("text").innerText;
+let textDefault = document.getElementById("text").innerText;
+text = text.slice(0, text.length).split(" ");
 var i = 0;
-var input = document.getElementById('poleVvode')
+var input = document.getElementById("poleVvode");
 
-function poleVvode() {
-    console.log(text[i])
-    if (text[i] == input.value) {
-        input.value = ''
-        i++
-       } 
-    setTimeout(() => {
-        var poleVvode = document.getElementById('poleVvode');
-        poleVvode.oninput = e => {
-            e.target.value = ''; 
-          }
-    }, 60000)
+let pudge = "Ну что как всегдаВАлл Пик захожу яПуджа выбираю";
+function poleVvode(inpt) {
+  console.log(text[i]);
+  console.log(inpt);
+  if (text[i] == input.value) {
+    input.value = "";
+    delete text[i];
+    document.getElementById("text").innerText = text.join(" ");
+    i++;
+  }
+  setTimeout(() => {
+    var poleVvode = document.getElementById("poleVvode");
+    poleVvode.setAttribute("disabled", "disabled");
+    input.value = "";
+  }, (inpt = 60000));
+}
+
+function poleRestart() {
+  document.getElementById("text").innerText = textDefault;
+  var poleVvode = document.getElementById("poleVvode");
+  poleVvode.removeAttribute("disabled");
+  input.value = "";
+}
+
+function filterShow(){
+  if(document.querySelector("#rus").hasAttribute("checked")){
+    document.getElementById("text").innerText = pudge;
+  }
 }
