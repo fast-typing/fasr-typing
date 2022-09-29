5dc851c7a100d5e7643e986ddef3e525ea2004f3
dead inside <82109250+fast-typing@users.noreply.github.com>
2022-09-29 17:52:09 +0400

    фильтры (время, язык), рабочий ввод, свм

--- filter.js
+++ filter.js
@@ -1,5 +1,4 @@
 function filterShowHide() {
-    console.log(document.getElementById('forms').style.display)
     if (document.getElementById('forms').style.display == 'none'){
         document.getElementById('forms').style.display = 'flex'
     } else {
--- type.js
+++ type.js
@@ -1,36 +1,67 @@
-var text = document.getElementById("text").innerText;
-let textDefault = document.getElementById("text").innerText;
-text = text.slice(0, text.length).split(" ");
+var text = document.getElementById('text').innerText;
+var inputTime = 60;
+var quanWords = 50;
+var textDefault = document.getElementById("text").innerText;
+var engText = "I think Pudge-Chen combo is really IMBALANCED! NaVi used this bug and won last game. First, this combo is not that hard for pro gamers. Second, this combo is different from the same one in war3 dota. Third, 2 normal skills shouldn't kill a hero with aegis easily. NICE BALANCE"
+var rusText = 'ПУДЖ ГОПНИК. пудж в кофточке и спортивках, не хотелось бы встретить такого на районе, иначе придется отдать своё мясо ему. ПУДЖ СПРОТСМЕН. Пудж настолько богат что покупает себе помимо мома костюмчик адидас. Что добавляет + 15 к скорости бега. ПУДЖ АНИМЕШНИК. Такую прическу хотят все ДЕВАЧКИ из доты. ПУДЖ ИМБА'
+text = text.split(' ');
+document.getElementById('poleVvode').setAttribute('autocomplete', 'off');
 var i = 0;
-var input = document.getElementById("poleVvode");
+var input = document.getElementById('poleVvode');
 
-let pudge = "Ну что как всегдаВАлл Пик захожу яПуджа выбираю";
-function poleVvode(inpt) {
-  console.log(text[i]);
-  console.log(inpt);
-  if (text[i] == input.value) {
-    input.value = "";
-    delete text[i];
-    document.getElementById("text").innerText = text.join(" ");
-    i++;
-  }
-  setTimeout(() => {
-    var poleVvode = document.getElementById("poleVvode");
-    poleVvode.setAttribute("disabled", "disabled");
-    poleVvode.setAttribute("placeholder", "time is over!");
-    input.value = "";
-  }, (inpt = 60000));
+function filter() {
+    inputTime = document.getElementById('time').value;
+    quanWords = document.getElementById('words').value;
+    if (document.getElementById('rus').checked == true) {
+        document.getElementById('text').innerText = rusText;
+        text = rusText.slice(0,engText.length).split(' ');
+    } else {
+        document.getElementById('text').innerText = engText;
+        text = engText.slice(0,engText.length).split(' ');
+    }
+    
 }
 
-function poleRestart() {
-  document.getElementById("text").innerText = textDefault;
-  var poleVvode = document.getElementById("poleVvode");
-  poleVvode.removeAttribute("disabled");
-  input.value = "";
+function poleVvode() {
+    // if (text.length > quanWords) {
+    //     text = text.slice()
+    // }
+    // var mas = input.value.split('')
+    // var length = mas.length - 1;
+    // console.log(text[i] + ' ' == input.value)
+    // console.log(mas[length] == ' ')
+    // mas[length] == ' '
+
+    if (text[i]  == input.value) {
+        input.value = '';
+        delete text[i];
+        document.getElementById("text").innerText = text.join(" ");
+        i++;
+       } 
+    if (inputTime == '') {
+        inputTime = 60
+    }
+    setTimeout(() => {
+        var poleVvode = document.getElementById('poleVvode');
+        poleVvode.value = '';
+        poleVvode.setAttribute('disabled', 'disabled');
+        poleVvode.setAttribute('placeholder', 'time is over!');
+        let wpm = Math.round(i/(inputTime/60));
+        console.log(`Words per minute ${wpm}`)
+        // let wpmBlock = document.createTextNode(`Word per minute: ${wpm}`);
+    }, inputTime*1000)
 }
 
-function filterShow(){
-  if(document.querySelector("#rus").hasAttribute("checked")){
-    document.getElementById("text").innerText = pudge;
-  }
+function restart() {
+    if (document.getElementById('rus').checked == true) {
+        document.getElementById('text').innerText = rusText;
+        text = rusText.slice(0,engText.length).split(' ');
+    } else {
+        document.getElementById('text').innerText = engText;
+        text = engText.slice(0,engText.length).split(' ');
+    }
+    var poleVvode = document.getElementById('poleVvode');
+    poleVvode.setAttribute('placeholder', 'write there');
+    poleVvode.removeAttribute('disabled');
+    i = 0;
 }
