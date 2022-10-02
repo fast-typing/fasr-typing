c273e89afad5ca14469a14d3c2c026356187d745
dead inside <82109250+fast-typing@users.noreply.github.com>
2022-10-02 21:46:15 +0400

    alert wpm, cpm

--- type.js
+++ type.js
@@ -1,15 +1,17 @@
 document.getElementById('poleVvode').setAttribute('autocomplete', 'off');
 var text = document.getElementById('text');
-var textDefault = document.getElementById("text").innerText;
 var input = document.getElementById('poleVvode');
+var textDefault = document.getElementById("text").innerText;
 var engText = ['pudge', 'function', 'document', 'element', 'bookmark', 'attribute', 'condition', 'accessing', 'temperature', 'notebook', 'birthday','imbalanced','cool','Obama','crash','computer','text','firewall','hardware','development','netiquette','equipment','install','search','update','stack','application','development','debug','delete','disconnect','enable','reboot','verify','compile','database','eject','folder','layout','namespace','outsource','password','upload','interface','backup','class','variable','property','response','current'];
 var rusText = ['пудж', 'ручка', 'модный', 'современный', 'пранк', 'цикада', 'кошка', 'узбекистак', 'таджик', 'микрофон', 'линейка', 'ластик', 'наушники', 'процессор', 'стол', 'полка', 'маркер', 'кубик', 'арбуз', 'черешня', 'фантик', 'лимонад', 'вирус', 'юбилей', 'работа', 'терпение', 'сковорода', 'хобот', 'утро', 'скакалка', 'цитрус', 'мигрень', 'йогурт', 'эскимо', 'жаргон', 'зарождение', 'пирог', 'ведро', 'лебедь', 'табуретка', 'тетрадка', 'тетрадка', 'чаепитие', 'работа', 'яблоко', 'шоколад', 'антиутопия', 'месяц', 'щегол', 'обводка'];
 var oldWords = [];
 let NowArr = [];
+let arrWords = [];
 var inputTime = 30;
 var inputWords = 30;
 var i = 0;
-
+let quanSymb = 0;
+var wpm;
 
 function randomArr(array) {
     array.sort(() => Math.random() - 0.5);
@@ -39,14 +41,18 @@ function filter( ) {
 
     if (inputWords > 50) {
         inputWords = 50;
+        document.getElementById('words').value = 50;
     } else if (inputWords < 20) {
         inputWords = 20;
+        document.getElementById('words').value = 20;
     }
 
     if (inputTime > 180) {
         inputTime = 180;
+        document.getElementById('time').value = 180;
     } else if (inputTime < 30) {
         inputTime = 30;
+        document.getElementById('time').value = 30;
     }
 
     if (document.getElementById('rus').checked == true) {
@@ -58,32 +64,42 @@ function filter( ) {
 
 
 function poleVvode() {
-    // if (text.length > quanWords) {
-    //     text = text.slice()
-    // }
-    // var mas = input.value.split('')
-    // var length = mas.length - 1;
-    // console.log(text[i] + ' ' == input.value)
-    // console.log(mas[length] == ' ')
-    // mas[length] == ' '
     if (NowArr[i] == input.value) {
         input.value = '';
         var word = document.createElement('span');
         word.style.opacity = '0.3'
         word.innerHTML = NowArr[i];
         oldWords.push(word.outerHTML);
+        arrWords.push(NowArr[i]);
         delete NowArr[i];
         document.getElementById("text").innerHTML = oldWords.join(' ') + NowArr.join(" ");
         i++;
     } 
 
+    if (i == 1){
+        timer();
+    }
+}
+
+function timer() {
     setTimeout(() => {
         var poleVvode = document.getElementById('poleVvode');
         poleVvode.value = '';
         poleVvode.setAttribute('disabled', 'disabled');
         poleVvode.setAttribute('placeholder', 'time is over!');
-        var wpm = Math.round(i/(inputTime/60));
-        console.log(`Words per minute ${wpm}`);
+        wpm = Math.round(i/(inputTime/60));
+        
+        
+        for (c = 0; c < i; c++) {
+            let word = arrWords[c];
+            quanSymb += word.length;
+        }
+
+        quanSymb = Math.round(quanSymb/(inputTime/60))
+
+        alert(`Words per minute ${wpm}`);
+        alert(`Characters per minute ${quanSymb}`);
+
     }, inputTime*1000);
 }
 
