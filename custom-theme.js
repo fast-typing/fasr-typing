ecf3c7dbb697ba826c44dbed7006657fafb125cd
dead inside <82109250+fast-typing@users.noreply.github.com>
2022-10-11 21:28:01 +0400

    eto ne kruto

--- custom-theme.js
+++ custom-theme.js
@@ -1,28 +1,49 @@
-let colorWell = document.getElementById('colorWell')
+let colorWell = document.getElementById('colorMainBG')
+let colorAddBG = document.getElementById('colorAddBG')
 let color1 = document.getElementById('color-1')
 let color2 = document.getElementById('color-2')
 let color3 = document.getElementById('color-3')
+let textColor = document.getElementById('textColor')
+let dot1 = document.getElementById('dot-1')
+let dot2 = document.getElementById('dot-2')
+let dot3 = document.getElementById('dot-3')
+let tag1 = document.getElementById('1')
+let tag2 = document.getElementById('2')
+let tag3 = document.getElementById('3')
+let tag4 = document.getElementById('4')
+let nav = document.getElementById('5')
 let body = document.getElementById('body')
 
 colorWell.addEventListener('input', e =>{
     body.style.backgroundColor = e.target.value
 })
 
-
 color1.addEventListener('input', e =>{
-    let dot1 = document.getElementById('dot-1')
     dot1.style.background = e.target.value
     body.style.setProperty('--link1Color', dot1.style.background)
 })
 
 color2.addEventListener('input', e =>{
-    let dot2 = document.getElementById('dot-2')
     dot2.style.background = e.target.value
     body.style.setProperty('--link2Color', dot2.style.background)
 })
 
 color3.addEventListener('input', e =>{
-    let dot3 = document.getElementById('dot-3')
     dot3.style.background = e.target.value
     body.style.setProperty('--link3Color', dot3.style.background)
 })
+
+colorAddBG.addEventListener('input', e =>{
+    tag1.style.background = e.target.value
+    tag2.style.background = e.target.value
+    tag3.style.background = e.target.value
+    tag4.style.background = e.target.value
+    nav.style.background = e.target.value
+}) 
+
+textColor.addEventListener('input', e =>{
+    tag1.style.color = e.target.value
+    tag2.style.color = e.target.value
+    tag3.style.color = e.target.value
+    tag4.style.color = e.target.value
+}) 
--- script.js
+++ script.js
@@ -3,7 +3,6 @@ function switchTheme1() {
     let theme = document.getElementById('theme')
     theme.href = 'theme-1.css'
     localStorage.setItem('test', 1)
-    
 }
 
 function switchTheme2() {
@@ -19,22 +18,87 @@ function switchTheme3() {
 }
 
 function switchTheme4() {
-    let theme = document.getElementById('theme')
-    theme.href = 'theme-4.css'
+    // let colorWell = document.getElementById('colorMainBG')
+    // let colorAddBG = document.getElementById('colorAddBG')
+    // let color1 = document.getElementById('color-1')
+    // let color2 = document.getElementById('color-2')
+    // let color3 = document.getElementById('color-3')
+    // let textColor = document.getElementById('textColor')
+    let dot1 = document.getElementById('dot-1')
+    let dot2 = document.getElementById('dot-2')
+    let dot3 = document.getElementById('dot-3')
+    // let tag1 = document.getElementById('1')
+    // let tag2 = document.getElementById('2')
+    // let tag3 = document.getElementById('3')
+    // let tag4 = document.getElementById('4')
+    let nav = document.getElementById('5')
+    let body = document.getElementById('body')
+
+    // colorWell.addEventListener('input', e =>{
+    //     body.style.backgroundColor = e.target.value
+    //     localStorage.setItem('a', e.target.value)
+    // })
+
+    // color1.addEventListener('input', e =>{
+    //     dot1.style.background = e.target.value
+    //     body.style.setProperty('--link1Color', dot1.style.background)
+    //     localStorage.setItem('b', e.target.value)
+    // })
+
+    // color2.addEventListener('input', e =>{
+    //     dot2.style.background = e.target.value
+    //     body.style.setProperty('--link2Color', dot2.style.background)
+    //     localStorage.setItem('c', e.target.value)
+    // })
+
+    // color3.addEventListener('input', e =>{
+    //     dot3.style.background = e.target.value
+    //     body.style.setProperty('--link3Color', dot3.style.background)
+    //     localStorage.setItem('d', e.target.value)
+    // })
+
+    // colorAddBG.addEventListener('input', e =>{
+    //     tag1.style.background = e.target.value
+    //     tag2.style.background = e.target.value
+    //     tag3.style.background = e.target.value
+    //     tag4.style.background = e.target.value
+    //     nav.style.background = e.target.value
+    //     localStorage.setItem('e', e.target.value)
+    // }) 
+
+    // textColor.addEventListener('input', e =>{
+    //     tag1.style.color = e.target.value
+    //     tag2.style.color = e.target.value
+    //     tag3.style.color = e.target.value
+    //     tag4.style.color = e.target.value
+    //     localStorage.setItem('f', e.target.value)
+    // }) 
     localStorage.setItem('test', 4)
+    body.style.backgroundColor = localStorage.getItem('a')
+    dot1.style.background = localStorage.getItem('b')
+    dot2.style.background = localStorage.getItem('c')
+    dot3.style.background = localStorage.getItem('d')
+    // tag1.style.color = localStorage.getItem('f')
+    // tag1.style.background = localStorage.getItem('e')
+    // tag2.style.color = localStorage.getItem('f')
+    // tag2.style.background = localStorage.getItem('e')
+    // tag3.style.color = localStorage.getItem('f')
+    // tag3.style.background = localStorage.getItem('e')
+    // tag4.style.color = localStorage.getItem('f')
+    // tag4.style.background = localStorage.getItem('e')
+    nav.style.background = localStorage.getItem('e')
+
 }
 
-
-if (localStorage.getItem('test') == 2){
-    let theme = document.getElementById('theme')
-    theme.href = 'theme-2.css'
-} else if (localStorage.getItem('test') == 3) {
-    let theme = document.getElementById('theme')
-    theme.href = 'theme-3.css'
-// } else if (localStorage.getItem('test') == 4) {
-//     let theme = document.getElementById('theme')
-//     theme.href = 'theme-4.css'
-} else {
+if (localStorage.getItem('test') == 1) {
     let theme = document.getElementById('theme')
     theme.href = 'theme-1.css'
+} else if (localStorage.getItem('test') == 2){
+    let theme = document.getElementById('theme')
+    theme.href = 'theme-2.css'
+} else if (localStorage.getItem('test') == 3){
+    let theme = document.getElementById('theme')
+    theme.href = 'theme-3.css'
+} else {
+    switchTheme4()
 }
